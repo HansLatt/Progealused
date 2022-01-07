d36020af8092bfac0f2f303f859f0e7011f0da55
Hans Lätt <95487176+HansLatt@users.noreply.github.com>
2022-01-07 09:08:55 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -2,13 +2,13 @@ sisend = prompt("Palun sisesta positiivne temperatuur 1-60-ni")
 
 function checkTemp() {
 if (sisend <= 20)
-    console.log("-1");
+    return -1;
 else if (sisend >= 21 && sisend <= 40)
-    console.log("0");
+    return 0;
 else if (sisend >= 41 && sisend <= 60)
-    console.log("1");
+    return 1;
 else
-    console.log("ERROR")
+    return ("ERROR")
 }
 
 checkTemp()
